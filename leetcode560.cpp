5d3206e8ba9e00c70e685df543d562ca7caf1454
lxs <3257512314@qq.com>
2020-05-15 18:34:54 +0800

    add leetcode560

---
+++ leetcode560.cpp
@@ -0,0 +1,14 @@
+class solution{
+public:
+    int subarraySum(vector<int>& nums, int k){
+        int count = 0;
+        for(int start = 0;start < nums.size();++start){
+            int sum = 0;
+            for(int end = start;end >= 0;end--){
+                sum += nums[end];
+                if(sum == k)count++;
+            }
+        }
+        return count;
+    }
+};
